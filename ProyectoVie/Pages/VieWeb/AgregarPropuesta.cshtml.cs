78b9cbd6cb6b7949389a0a3eb3281e8eee9eea38
ChepeTEC <jo.gonzalez@estudiantec.cr>
2024-10-06 21:49:33 -0600

    CRUD Propuestas Interfaz
    Aun falta unirlo como segunda capa del backend.

---
+++ ProyectoVie/Pages/VieWeb/AgregarPropuesta.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace ProyectoVie.Pages.VieWeb
+{
+    public class AgregarPropuestaModel : PageModel
+    {
+        [BindProperty]
+        public Propuesta Proposal { get; set; } = new Propuesta();
+
+        public class Propuesta
+        {
+            public string NumeroAcuerdo { get; set; }
+            public DateTime FechaAprobacion { get; set; }
+            public string NombreProyecto { get; set; }
+            public string TipoExtension { get; set; }
+            public string ObjetivoDesarrolloSostenible { get; set; }
+            public string NombreExtensionista { get; set; }
+            public string InstitucionAcademica { get; set; }
+            public string TipoNombramiento { get; set; }
+            public string Condicion { get; set; }
+            public string PeriodoParticipacion { get; set; }
+            public string NombreContacto { get; set; }
+            public string Organizacion { get; set; }
+            public string Contacto { get; set; }
+            public string DescripcionPropuesta { get; set; }
+        }
+
+        public void OnGet()
+        {
+            // Inicializa el modelo si es necesario
+            Proposal = new Propuesta
+            {
+                NumeroAcuerdo = "12-2017",
+                FechaAprobacion = DateTime.Now
+            };
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+
+            return RedirectToPage("Index"); // Redirige a la página principal después de guardar
+        }
+    }
+}
