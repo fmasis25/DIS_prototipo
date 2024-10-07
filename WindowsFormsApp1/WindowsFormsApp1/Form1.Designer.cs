fa415e345b2f5767385087020d4240dc6358ed9c
Alejandro Gutiérrez <alejandrogutierrezchaves@gmail.com>
2024-10-07 15:50:13 -0600

    graficos

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.EscuelasPorPropuesta = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.PropuestasPorEstadoChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.EscuelasPorEstadoChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.NotaPromedioChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.EscuelasPorPropuesta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PropuestasPorEstadoChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EscuelasPorEstadoChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NotaPromedioChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // EscuelasPorPropuesta
+            // 
+            chartArea1.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.AxisY.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.Name = "ChartArea1";
+            this.EscuelasPorPropuesta.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.EscuelasPorPropuesta.Legends.Add(legend1);
+            this.EscuelasPorPropuesta.Location = new System.Drawing.Point(0, 12);
+            this.EscuelasPorPropuesta.Name = "EscuelasPorPropuesta";
+            this.EscuelasPorPropuesta.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Grayscale;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.EscuelasPorPropuesta.Series.Add(series1);
+            this.EscuelasPorPropuesta.Size = new System.Drawing.Size(726, 485);
+            this.EscuelasPorPropuesta.TabIndex = 0;
+            this.EscuelasPorPropuesta.Click += new System.EventHandler(this.EscuelasPorPropuesta_Click);
+            // 
+            // PropuestasPorEstadoChart
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.PropuestasPorEstadoChart.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.PropuestasPorEstadoChart.Legends.Add(legend2);
+            this.PropuestasPorEstadoChart.Location = new System.Drawing.Point(0, 553);
+            this.PropuestasPorEstadoChart.Name = "PropuestasPorEstadoChart";
+            this.PropuestasPorEstadoChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.PropuestasPorEstadoChart.Series.Add(series2);
+            this.PropuestasPorEstadoChart.Size = new System.Drawing.Size(625, 280);
+            this.PropuestasPorEstadoChart.TabIndex = 1;
+            this.PropuestasPorEstadoChart.Text = "chart1";
+            this.PropuestasPorEstadoChart.Click += new System.EventHandler(this.PropuestasPorEstadoChart_Click);
+            // 
+            // EscuelasPorEstadoChart
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.EscuelasPorEstadoChart.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.EscuelasPorEstadoChart.Legends.Add(legend3);
+            this.EscuelasPorEstadoChart.Location = new System.Drawing.Point(732, 12);
+            this.EscuelasPorEstadoChart.Name = "EscuelasPorEstadoChart";
+            this.EscuelasPorEstadoChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Excel;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedBar;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.EscuelasPorEstadoChart.Series.Add(series3);
+            this.EscuelasPorEstadoChart.Size = new System.Drawing.Size(961, 485);
+            this.EscuelasPorEstadoChart.TabIndex = 2;
+            this.EscuelasPorEstadoChart.Text = "EscuelasPorEstadoChart";
+            // 
+            // NotaPromedioChart
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.NotaPromedioChart.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.NotaPromedioChart.Legends.Add(legend4);
+            this.NotaPromedioChart.Location = new System.Drawing.Point(646, 503);
+            this.NotaPromedioChart.Name = "NotaPromedioChart";
+            this.NotaPromedioChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Grayscale;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bar;
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.NotaPromedioChart.Series.Add(series4);
+            this.NotaPromedioChart.Size = new System.Drawing.Size(1013, 413);
+            this.NotaPromedioChart.TabIndex = 3;
+            this.NotaPromedioChart.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1671, 969);
+            this.Controls.Add(this.NotaPromedioChart);
+            this.Controls.Add(this.EscuelasPorEstadoChart);
+            this.Controls.Add(this.PropuestasPorEstadoChart);
+            this.Controls.Add(this.EscuelasPorPropuesta);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.EscuelasPorPropuesta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PropuestasPorEstadoChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EscuelasPorEstadoChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NotaPromedioChart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart EscuelasPorPropuesta;
+        private System.Windows.Forms.DataVisualization.Charting.Chart PropuestasPorEstadoChart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart EscuelasPorEstadoChart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart NotaPromedioChart;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void EscuelasPorPropuesta_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=tcp:serverprogra.database.windows.net,1433;Initial Catalog=VIE;Persist Security Info=False;User ID=Prograadmin;Password=proyectoVIE123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Llamar al procedimiento almacenado para obtener los datos de escuelas
+                    SqlCommand command = new SqlCommand("sp_ContarEscuelasPorPropuesta", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+
+                    // Limpiar series anteriores
+                    EscuelasPorPropuesta.Series.Clear();
+
+                    // Crear nueva serie de columnas
+                    Series series = new Series("Suma Total Escuelas");
+                    series.ChartType = SeriesChartType.Column;
+                    EscuelasPorPropuesta.Series.Add(series);
+
+                    // Cargar datos en la serie
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        series.Points.AddXY(row["Escuela"].ToString(), Convert.ToInt32(row["SumaTotalEscuelas"]));
+                    }
+
+                    // Configurar el área del gráfico
+                    var chartArea = EscuelasPorPropuesta.ChartAreas[0];
+
+                    // Personalizar el título de los ejes
+                    chartArea.AxisX.Title = "Escuela";
+                    chartArea.AxisX.TitleFont = new Font("Arial", 14, FontStyle.Bold); // Título en negrita y mayor tamaño
+                    chartArea.AxisY.Title = "Suma Total Escuelas";
+                    chartArea.AxisY.TitleFont = new Font("Arial", 14, FontStyle.Bold); // Título en negrita y mayor tamaño
+
+                    // Rotar las etiquetas del eje X a vertical
+                    chartArea.AxisX.LabelStyle.Angle = -90;
+
+                    // Asegurar que todas las etiquetas se muestren
+                    chartArea.AxisX.Interval = 1;
+
+                    // Mostrar todos los números en el eje Y
+                    chartArea.AxisY.Interval = 1;
+
+                    // Asegurar que el eje Y comience en 0
+                    chartArea.AxisY.IsStartedFromZero = true;
+
+                    // Eliminar la cuadrícula de fondo
+                    chartArea.AxisX.MajorGrid.Enabled = false;
+                    chartArea.AxisY.MajorGrid.Enabled = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void PropuestasPorEstado_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=tcp:serverprogra.database.windows.net,1433;Initial Catalog=VIE;Persist Security Info=False;User ID=Prograadmin;Password=proyectoVIE123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Llamar al procedimiento almacenado para obtener los datos de propuestas por estado
+                    SqlCommand command = new SqlCommand("sp_ContarPropuestasPorEstado", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+
+                    // Limpiar las series anteriores
+                    PropuestasPorEstadoChart.Series.Clear();
+
+                    // Crear una nueva serie para el gráfico circular
+                    Series series = new Series("PropuestasPorEstado");
+                    series.ChartType = SeriesChartType.Pie; // Gráfico circular
+                    PropuestasPorEstadoChart.Series.Add(series);
+
+                    // Cargar los datos en el gráfico
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        // Agregar datos al gráfico circular (nombre del estado y cantidad de propuestas)
+                        series.Points.AddXY(row["EstadoProyecto"].ToString(), Convert.ToInt32(row["CantidadPropuestas"]));
+                    }
+
+                    // Personalizar los colores del gráfico circular
+                    Color[] colores = {
+                    
+                    Color.FromArgb(220, 20, 60),  // Rojo intenso
+                    Color.FromArgb(128, 128, 128), // Gris medio
+                    Color.FromArgb(0, 51, 102), // Azul oscuro
+                    Color.FromArgb(192, 192, 192)// Gris claro
+                    
+                    
+                    };
+
+                    // Asignar colores personalizados a cada punto de datos
+                    for (int i = 0; i < series.Points.Count; i++)
+                    {
+                        series.Points[i].Color = colores[i % colores.Length];
+                    }
+
+                    // Personalizar el gráfico
+                    series["PieLabelStyle"] = "Outside"; // Mostrar las etiquetas fuera del gráfico
+                    series.Label = "#PERCENT{P1}";       // Mostrar porcentaje en etiquetas
+                    series.LegendText = "#VALX";          // Nombre del estado como leyenda
+
+                    // Opcional: Personalización del gráfico
+                    PropuestasPorEstadoChart.Titles.Clear();
+                    PropuestasPorEstadoChart.Titles.Add("Cantidad de Propuestas por Estado de Proyecto");
+                    PropuestasPorEstadoChart.Titles[0].Font = new Font("Arial", 14, FontStyle.Bold);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            EscuelasPorPropuesta_Click(this, EventArgs.Empty);
+            PropuestasPorEstado_Click(this, EventArgs.Empty); // Llama a la carga inicial del gráfico
+            EscuelasPorEstado_Click(this, EventArgs.Empty);
+            NotaPromedioPorEscuela_Click(this, EventArgs.Empty);
+        }
+
+
+        private void PropuestasPorEstadoChart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EscuelasPorEstado_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=tcp:serverprogra.database.windows.net,1433;Initial Catalog=VIE;Persist Security Info=False;User ID=Prograadmin;Password=proyectoVIE123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Llamar al procedimiento almacenado
+                    SqlCommand command = new SqlCommand("sp_ContarEscuelasPorEstado", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+
+                    // Limpiar las series anteriores y la leyenda
+                    EscuelasPorEstadoChart.Series.Clear();
+                    EscuelasPorEstadoChart.Legends.Clear(); // Limpia leyendas anteriores para evitar múltiples leyendas
+
+                    // Crear una nueva leyenda personalizada
+                    Legend legend = new Legend("Estados");
+                    legend.Docking = Docking.Right;
+                    EscuelasPorEstadoChart.Legends.Add(legend);
+
+                    // Definir colores personalizados (azul oscuro, rojo intenso, gris claro, gris medio)
+                    Color[] colores = { Color.FromArgb(0, 51, 102), // Azul oscuro
+                        Color.FromArgb(192, 192, 192), // Gris claro
+                        Color.FromArgb(220, 20, 60),// Rojo intenso
+                        Color.FromArgb(128, 128, 128)}; // Gris medio
+
+                    // Control para asignar colores a las series
+                    int colorIndex = 0;
+
+                    // Agrupar los datos por escuela y agregar series para cada estado de proyecto
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        string escuela = row["Escuela"].ToString();
+                        string estadoProyecto = row["EstadoProyecto"].ToString();
+                        int cantidadPropuestas = Convert.ToInt32(row["TotalPropuestas"]);
+
+                        // Solo continuar si la cantidad de propuestas es mayor que 0
+                        if (cantidadPropuestas > 0)
+                        {
+                            // Verificar si ya existe una serie para este estado
+                            Series series = EscuelasPorEstadoChart.Series.FindByName(estadoProyecto);
+                            if (series == null)
+                            {
+                                // Crear una nueva serie para este estado
+                                series = new Series(estadoProyecto);
+                                series.ChartType = SeriesChartType.StackedBar; // Gráfico de barras apiladas
+                                series.IsValueShownAsLabel = true; // Mostrar los valores encima de las barras
+
+                                // Asignar el color desde la lista
+                                series.Color = colores[colorIndex % colores.Length];
+                                colorIndex++; // Cambiar el color para la siguiente serie
+
+                                EscuelasPorEstadoChart.Series.Add(series);
+                            }
+
+                            // Agregar el valor a la serie
+                            series.Points.AddXY(escuela, cantidadPropuestas);
+                        }
+                    }
+
+                    // Configurar el área del gráfico
+                    var chartArea = EscuelasPorEstadoChart.ChartAreas[0];
+
+                    // Personalizar los ejes
+                    chartArea.AxisX.Title = "Escuela";
+                    chartArea.AxisX.TitleFont = new Font("Arial", 14, FontStyle.Bold);
+                    chartArea.AxisY.Title = "Cantidad de Propuestas";
+                    chartArea.AxisY.TitleFont = new Font("Arial", 14, FontStyle.Bold);
+
+                    // Mostrar todas las etiquetas del eje X
+                    chartArea.AxisX.Interval = 1;
+                    chartArea.AxisX.LabelStyle.Angle = -90; // Etiquetas en vertical
+
+                    // Mostrar los números completos en el eje Y
+                    chartArea.AxisY.Interval = 1;
+
+                    // Personalización adicional
+                    EscuelasPorEstadoChart.Titles.Clear();
+                    EscuelasPorEstadoChart.Titles.Add("Propuestas por Escuela y Estado de Proyecto");
+                    EscuelasPorEstadoChart.Titles[0].Font = new Font("Arial", 16, FontStyle.Bold);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NotaPromedioPorEscuela_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=tcp:serverprogra.database.windows.net,1433;Initial Catalog=VIE;Persist Security Info=False;User ID=Prograadmin;Password=proyectoVIE123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Llamar al procedimiento almacenado
+                    SqlCommand command = new SqlCommand("sp_NotaPromedioPorEscuela", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+
+                    // Limpiar las series anteriores
+                    NotaPromedioChart.Series.Clear();
+
+                    // Crear una nueva serie para el gráfico de barras
+                    Series series = new Series("Nota Promedio");
+                    series.ChartType = SeriesChartType.Bar; // Gráfico de barras
+                    NotaPromedioChart.Series.Add(series);
+
+                    // Cargar datos en la serie
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        series.Points.AddXY(row["Escuela"].ToString(), Convert.ToDouble(row["NotaPromedio"]));
+                    }
+
+                    // Personalizar el gráfico
+                    series.Color = Color.FromArgb(0, 51, 102); // Azul oscuro
+
+                    // Configurar el área del gráfico
+                    var chartArea = NotaPromedioChart.ChartAreas[0];
+
+                    // Personalizar los ejes
+                    chartArea.AxisX.Title = "Escuela";
+                    chartArea.AxisX.TitleFont = new Font("Arial", 14, FontStyle.Bold);
+                    chartArea.AxisY.Title = "Nota Promedio";
+                    chartArea.AxisY.TitleFont = new Font("Arial", 14, FontStyle.Bold);
+
+                    // Mostrar todas las etiquetas del eje X
+                    chartArea.AxisX.Interval = 1;
+                    chartArea.AxisX.LabelStyle.Angle = -90; // Etiquetas en vertical
+
+                    // Personalización adicional (opcional)
+                    NotaPromedioChart.Legends[0].Enabled = true; // Habilitar la leyenda
+                    NotaPromedioChart.Titles.Clear();
+                    NotaPromedioChart.Titles.Add("Nota Promedio de Propuestas por Escuela");
+                    NotaPromedioChart.Titles[0].Font = new Font("Arial", 16, FontStyle.Bold);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+    }
+}
+
